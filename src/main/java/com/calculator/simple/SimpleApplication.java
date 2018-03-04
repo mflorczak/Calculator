207b4e327acd47f38cf29be8e511fc35ffd191c0
mflorczak <michal.florczak0@gmail.com>
2018-03-04 11:14:00 +0100

    Third commit

--- src/main/java/com/calculator/simple/SimpleApplication.java
+++ src/main/java/com/calculator/simple/SimpleApplication.java
@@ -10,27 +10,5 @@ public class SimpleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleApplication.class, args);
-
-		/*int n;
-		System.out.println("Dodawanie wybierz 1\nOdejmowanie wybierz 2");
-        try (Scanner in = new Scanner(System.in)) {
-            n = in.nextInt();
-        }
-
-        switch (n) {
-           case 1:
-                Calculator calculator = new Calculator(10, 5);
-                calculator.additionXFromY();
-                System.out.println(calculator);
-                break;
-            case 2:
-                calculator = new Calculator(10, 31);
-                calculator.subtractionXFromY();
-                System.out.println(calculator);
-                break;
-            default:
-                System.out.println("Nie ma takiej opcji");
-                break;
-        }*/
 	}
 }
